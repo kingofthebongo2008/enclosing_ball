813c7e9b69007ba411fcb2ae98d3331e6efb87bf
Stefan Dyulgerov <stefan.dyulgerov@ubisoft.com>
2018-06-14 16:22:24 +0300

    fixed

--- enclosing_ball/src/hello_triangle/enclosing_ball.cpp
+++ enclosing_ball/src/hello_triangle/enclosing_ball.cpp
@@ -1,9 +1,13 @@
 #include "pch.h"
 #include <cstdint>
 
+#include "enclosing_ball.h"
+
 namespace computational_geometry
 {
-
+    sphere enclosing_ball(const std::vector<point3>& points)
+    {
+        sphere r;
+        return r;
+    }
 }
-
-
--- enclosing_ball/src/hello_triangle/enclosing_ball.h
+++ enclosing_ball/src/hello_triangle/enclosing_ball.h
@@ -1,6 +1,23 @@
 #pragma once
 
+#include <vector>
+
 namespace computational_geometry
 {
+    struct point3
+    {
+        float m_x = 0.0f;
+        float m_y = 0.0f;
+        float m_z = 0.0f;
+    };
+
+    struct sphere
+    {
+        point3 m_center;
+        float  m_radius = 0.0f;;
+    };
+
+
+    sphere enclosing_ball(const std::vector<point3>& points);
 
 }
